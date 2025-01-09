46ba02aa43557a4b325e24d1cfcf7bdb085e431e
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2025-01-09 16:39:25 +0530

    Page why us added

--- Cellnfra/Controllers/HomeController.cs
+++ Cellnfra/Controllers/HomeController.cs
@@ -18,15 +18,14 @@ namespace Cellnfra.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult WhyUs()
+        {
+            return View();
+        }
+        public IActionResult ElectronicsRepairs()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
